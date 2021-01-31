1e2050128a77dc2ff41125a89ec2647992b5c52e
danielsespitia <63252057+danielsespitia@users.noreply.github.com>
2021-01-31 14:57:55 -0500

    stylized home page (#3)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,41 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  height: 3rem;
+`;
+
+export const LoginButton = styled.div`
+  align-self: center;
+  margin-right: 1rem;
+`;
+
+export const Body = styled.div`
+  height: 50rem;
+  margin: auto;
+`;
+
+export const SignupButton = styled.div`
+  align-self: center;
+`;
+
 function Home() {
   return (
     <div className="home">
-      <h1>Inicio</h1>
+      <Header className="header">
+        <LoginButton>
+          <button>Iniciar Sesion</button>
+        </LoginButton>
+      </Header>
+      <Body className="body">
+        <div>
+          <h1>Descubre musicos cerca</h1>
+        </div>
+        <SignupButton>
+          <button>Registrate</button>
+        </SignupButton>
+      </Body>
     </div>
   );
 }
