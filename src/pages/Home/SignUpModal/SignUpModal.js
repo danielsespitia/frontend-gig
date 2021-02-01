9eda64198d606651c61ca0c69c1a9d6b449a3744
Daniel Espitia <dsespitiac@gmail.com>
2021-01-31 22:45:43 -0500

    created log in auth and set errors for sign up

--- src/pages/Home/SignUpModal/SignUpModal.js
+++ src/pages/Home/SignUpModal/SignUpModal.js
@@ -1,9 +1,90 @@
+import { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../../../store/AuthContext";
+import axios from "axios";
+
 import { SignUpForm } from "../../../components/Modals/SignUpForm/SignUpForm";
 
 function SignUpModal() {
+  const history = useHistory();
+  const { isAuthenticated } = useContext(AuthContext);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [terms, setTerms] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value, checked } = e.target;
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "confirmPassword":
+        setConfirmPassword(value);
+        break;
+      case "terms":
+        setTerms(checked);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (validate()) {
+      try {
+        const {
+          data: { token },
+        } = await axios({
+          method: "POST",
+          baseURL: process.env.REACT_APP_SERVER_URL,
+          url: `/users/sign-up`,
+          data: { name, password, email, terms },
+        });
+        localStorage.setItem("token", token);
+        const pathUser = "app/profile/edit";
+        localStorage.setItem("pathUser", pathUser);
+        isAuthenticated(token, pathUser);
+        history.push(`${pathUser}`);
+      } catch (err) {
+        setErrors({ account: "No se pudo crear tu cuenta" });
+      }
+    }
+  };
+
+  const validate = () => {
+    const arePasswordEqual =
+      !!password && !!confirmPassword && password === confirmPassword;
+
+    if (!arePasswordEqual) {
+      setErrors({ password: "La contraseñas no coinciden!" });
+      return false;
+    }
+    return true;
+  };
+
   return (
     <>
-      <SignUpForm></SignUpForm>
+      <SignUpForm
+        name={name}
+        password={password}
+        confirmPassword={confirmPassword}
+        email={email}
+        terms={terms}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        errorsPassword={errors.password}
+        errorsAccount={errors.account}
+      />
     </>
   );
 }
