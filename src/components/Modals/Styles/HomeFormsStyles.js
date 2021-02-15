76ae3513dfba9073263610b2f00ab588ede8039e
Daniel Espitia <dsespitiac@gmail.com>
2021-02-15 10:51:22 -0500

    distributed styles for home modal forms

---
+++ src/components/Modals/Styles/HomeFormsStyles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: grid;
+`;
+
+export const Error = styled.span`
+  font-size: 12px;
+  color: grey;
+  font-style: italic;
+`;
+
+export const ButtonSpan = styled.span`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+export const ItemContainer = styled.span`
+  display: inherit;
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
+
+export const TermsContainer = styled.span`
+  display: inherit;
+  align-items: center;
+  justify-content: center;
+  font-size: 11px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
+
+export const Span = styled.span`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+`;
